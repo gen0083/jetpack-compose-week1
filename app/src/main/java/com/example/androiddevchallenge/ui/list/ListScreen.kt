d4502e55bd85be6fec460bf2399f9bc8670a03ea
Gen <archiherewego@gmail.com>
2021-03-03 10:09:01 +0900

    List表示するアイテムを作成
    どうでもいいが、アイコンの参照とかの使い方がわからなくて困る
    SampleをAndroid Studioから見れれば楽になりそうなんだが・・・

--- app/src/main/java/com/example/androiddevchallenge/ui/list/ListScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/list/ListScreen.kt
@@ -1,8 +1,28 @@
 package com.example.androiddevchallenge.ui.list
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.NoPhotography
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.data.Pets
+import com.example.androiddevchallenge.data.generateRandomPets
 
 @Composable
 fun ListScreen() {
@@ -13,4 +33,36 @@ fun ListScreen() {
 @Preview(showSystemUi = true)
 fun ListScreenPreview() {
     ListScreen()
+}
+
+@Composable
+fun PetsItem(pet: Pets) {
+    Row(
+        modifier = Modifier.fillMaxWidth(1f),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Image(
+            imageVector = Icons.Default.NoPhotography,
+            contentDescription = "no image",
+            modifier = Modifier
+                .padding(8.dp)
+                .size(40.dp)
+                .background(Color.LightGray)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column(modifier = Modifier.weight(1f)) {
+            Text(
+                text = pet.name,
+                style = MaterialTheme.typography.h3,
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun PetsItemPreview() {
+    val pet = generateRandomPets()
+    PetsItem(pet = pet)
 }
